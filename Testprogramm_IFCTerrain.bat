0d6590cb211138b2ed80a1d26fc4b3aa32e137da
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2020-12-20 15:16:00 +0100

    Update: Developer Testing Software IFCTerrain(Command) - new: sample for Geograf Out - Format - changed: folder structure & names

--- Testprogramm_IFCTerrain.bat
+++ Testprogramm_IFCTerrain.bat
@@ -8,44 +8,49 @@ echo Testprogramm fuer Entwickler des IFCTerrain gestartet...
 echo ==================================================================
 echo.
 echo ========================01_LAND_XML===============================
-echo Testdatei fuer Land XML wird erzeugt:
-start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\06_JSON\sample_json_landxml_ifc4_tfs.json"
-echo LandXML (TriangulatedFaceSet): IFC4 erzeugt
-start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\06_JSON\sample_json_landxml_ifc4_sbsm.json"
-echo LandXML (ShellBasesSurfaceModel): IFC4 erzeugt
+echo Testdatei fuer Land XML wird NICHT erzeugt!
+rem start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\99_JSON\sample_json_landxml_ifc4_tfs.json"
+rem echo LandXML (TriangulatedFaceSet): IFC4 erzeugt
+rem start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\99_JSON\sample_json_landxml_ifc4_sbsm.json"
+rem echo LandXML (ShellBasesSurfaceModel): IFC4 erzeugt
 echo ==================================================================
 echo.
 echo ========================02_CityGML================================
 echo Testdatei fuer CityGML wird erzeugt:
-start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\06_JSON\sample_json_citygml_ifc4_tfs.json"
+start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\99_JSON\02_citygml_ifc4_tfs.json"
 echo CityGML (TriangulatedFaceSet): IFC4 erzeugt
 echo ==================================================================
 echo.
 echo ========================03_DXF====================================
 echo Testdatei fuer DXF wird erzeugt:
-start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\06_JSON\sample_json_dxf_ifc4_sbsm.json"
+start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\99_JSON\03_dxf_ifc4_sbsm.json"
 echo DXF (SBSM): IFC4 erzeugt
 echo ==================================================================
 echo.
 echo ========================04_RASTER_XYZ=============================
 echo Testdatei fuer Rasterdaten wird erzeugt:
-start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\06_JSON\sample_json_raster_ifc4_tfs.json"
-echo Raster (TriangulatedFaceSet): IFC4 erzeugt
-start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\06_JSON\sample_json_raster_ifc4_gcs.json"
-echo Raster (GeometricCurveSet): IFC4 erzeugt
-start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\06_JSON\sample_json_raster_ifc2x3_tfs.json"
+start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\99_JSON\04_01_raster_ifc2x3_tfs.json"
 echo Raster (TriangulatedFaceSet): IFC2x3 erzeugt
+start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\99_JSON\04_02_raster_ifc4_tfs.json"
+echo Raster (TriangulatedFaceSet): IFC4 erzeugt
 echo ==================================================================
 echo.
 echo ========================05_REB====================================
 echo Testdatei fuer REB-Daten wird erzeugt:
-start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\06_JSON\sample_json_reb_ifc4_tfs.json"
+start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\99_JSON\05_reb_ifc4_tfs.json"
+echo REB (TriangulatedFaceSet): IFC4 erzeugt
+echo ==================================================================
+echo.
+echo ========================06_Geograf OUT=============================
+echo Testdatei fuer REB-Daten wird erzeugt:
+start "" IFCTerrainCommand\bin\x64\Debug\IFCTerrainCommand.exe "IFCTerrainTestData\99_JSON\06_geograf_out_ifc4_tfs.json"
 echo REB (TriangulatedFaceSet): IFC4 erzeugt
 echo ==================================================================
 echo.
 echo ==================================================================
-echo Die Ergebnisse sind im Ordner: 'IFCTerrainTestData/07_Export' zu finden.
-start explorer "%CD%\IFCTerrainTestData\07_Export"
+echo Die Ergebnisse (6 Dateien) sind im Ordner: 
+echo 'IFCTerrainTestData/07_Export' zu finden.
+start explorer "%CD%\IFCTerrainTestData\00_Export"
 echo ==================================================================
 pause
 rem der nachfolgende Kommand schließt die IFCTerrainCommand.exe, sodass nicht ausführbare Prozesse nicht weiter im Hintergrund ausgeführt werden!
