005ba343265ccf22d064d253da917fd1ffe565b1
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-02-03 13:37:02 +0100

    Update GEOgraf Out - new reader using TIN instead of Mesh

---
+++ IFCTerrain/Model/Read/GEOgraf/OHorizon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFCTerrain.Model.Read.GEOgraf
+{
+    public class OHorizon
+    {
+        //Dokumentation
+        #region Documenation
+        //Link: https://vps2.hhk.de/geograf/Hilfe/GEOgraf/Anhang/Formatdokumentation/GRAFBAT_Format/GRAFBAT_Format.htm#Horizontdefinition
+        #endregion
+
+        //Konstruktor
+        #region Constructor
+        public OHorizon(int HorNr, string Designation, bool is3D)
+        {
+            this.HorNr = HorNr;
+            this.Designation = Designation;
+            this.is3D = is3D;
+        }
+        #endregion
+
+        //Eigenschaften
+        #region Properties
+        //Horizonnummer
+        public int HorNr { get; set; }
+        //Beschreibung des Horizonts
+        public string Designation { get; set; }
+        //Boolscher Wert - true = 3D-Horizont false = 2D-Horizont
+        public bool is3D { get; set; }
+        #endregion
+
+        //Methoden
+        #region Methods
+
+        #endregion
+    }
+}
